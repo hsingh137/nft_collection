403456a6451221c4a0cf2c59ac06ccf026f78b76
himanshus-ps <87009466+himanshus-ps@users.noreply.github.com>
2022-04-25 14:49:01 +0100

    added smart contracts

---
+++ hard-hat/contracts/CryptoDevs.sol
@@ -0,0 +1,10 @@
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.0;
+
+// inmporting ERC721 Enumerable 
+  import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+// importing Ownable 
+  import "@openzeppelin/contracts/access/Ownable.sol";
+  import "./IWhitelist.sol";
+
---
+++ hard-hat/contracts/IWhitelist.sol
@@ -0,0 +1,11 @@
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.0;
+
+
+ interface IWhitelist {
+    //  importing the mapping of whitelisted addresses from the other contract
+     function whitelistedAddresses(address) external view returns(bool);
+ }
+
+
+
