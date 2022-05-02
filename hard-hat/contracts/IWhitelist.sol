743d86e6c08a48fc1095079b0b91db442fe94626
himanshus-ps <87009466+himanshus-ps@users.noreply.github.com>
2022-05-02 22:47:46 +0100

    change

--- hard-hat/contracts/IWhitelist.sol
+++ hard-hat/contracts/IWhitelist.sol
@@ -5,6 +5,7 @@
  interface IWhitelist {
     //  importing the mapping of whitelisted addresses from the other contract
      function whitelistedAddresses(address) external view returns(bool);
+    //  this will check if the addres was white listed.
  }
 
 
